f8a635e4975c8588426881e18e43fc9020bd2d77
seifdang <77392896+seifdang@users.noreply.github.com>
2022-05-06 23:35:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,106 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const multer = require("multer")
-const cors = require("cors");
-const connection = require("./db");
-const userRoutes = require('./routes/users');
-const authRoutes = require('./routes/auth');
-const membRoutes = require('./routes/members');
-const visitRoutes = require('./routes/visits');
-const ReunionRoutes = require('./routes/reunion');
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./styles.module.css";
+import {HeroBg,Img} from "./ReuElem"
+import humanrights from "../images/humanrights.jpg"
+import { Button1 } from "../ButtonElements"
 
 
+	
+	
+    const Signup = () => {
 
+        const [firstName,setFirstName] = useState("");
+        const [lastName,setLastName] = useState("");
+        const [position,setPosition] = useState("");
+        const [file,setFile] = useState(null);
+    
 
-// database connection
-connection();
-
-// middlewares
-app.use(express.json());
-app.use(cors());
-
-//upload
-
-const storage = multer.diskStorage({
-    destination:(req,file,cb) =>{
-        cb(null,'public')
-    },
-    filename:(req,file,cb) =>{
-        cb(null,Date.now() +'-' + file.originalname)
-    }
-});
-
-const upload = multer({storage}).single('file');
-app.post('/upload', (req,res)=>  {
-    upload(req,res, (err)  => {
-        if(err){
-            return res.status(500).json(err)
+       
+        const [error, setError] = useState("");
+        const navigate = useNavigate();
+    
+       
+    
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            console.log(file)
+           let data={firstName:firstName,
+                lastName:file,
+                position:position,
+                
         }
-        return res.status(200).send(req.file)
-        
-})
-})
+            try {
+                const url = "http://localhost:8080/api/reunions/reunion ";
+                const { data: res } = await axios.post(url, data);
+                // navigate("/reunion");
+                console.log(res.message);
+            } catch (error) {
+                if (
+                    error.response &&
+                    error.response.status >= 400 &&
+                    error.response.status <= 500
+                ) {
+                    setError(error.response.data.message);
+                }
+            }
+        };
+    
+        return (
+            <>
+            
+            
+            
+            
+            
+            <div className={styles.signup_container}>
+                <div className={styles.signup_form_container}>
+                   
+                    <div className={styles.right}>
+                        <form className={styles.form_container} onSubmit={handleSubmit}>
+                            <h1>اضف اجتماع</h1>
+                            <input
+                                type="text"
+                                placeholder="عنوان الاجتماع"
+                                name="firstName"
+                                onChange={(x)=>setFirstName(x.target.value)}
+                                value={firstName}
+                                required
+                                className={styles.input}
+                            />
+                           
+                            <input
+                                type="date"
+                                placeholder="المصلحة"
+                                name="المصلحة"
+                                onChange={(x)=>setPosition(x.target.value)}
+                                value={position}
+                                required
+                                className={styles.input}
+                            />
+                             <input
+                                type="file"
+                                placeholder="التقرير"
+                                name="file"
+                                onChange={(x)=>setFile(x.target.value)}
+                                value={file}
+                                required
+                                className={styles.input}
+                                
+                            />
+                           
 
-// routes
-app.use("/api/users",userRoutes);
-app.use("/api/auth",authRoutes);
-app.use("/api/members",membRoutes);
-app.use("/api/visits",visitRoutes);
-app.use("/api/reunions",ReunionRoutes);
-
-
-const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
+                            {error && <div className={styles.error_msg}>{error}</div>}
+                            <button type="submit" className={styles.green_btn}>
+						التسجيل
+						</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            </>
+        );
+    };
+    
+    export default Signup;
